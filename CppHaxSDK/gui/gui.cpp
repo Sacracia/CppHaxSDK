c06fd6e6a641f2ca4d07dd4a9406353f3f2980d0
Sacracia <tiger3551999@mail.ru>
2024-11-01 03:22:39 +0300

    feat: directx10

--- CppHaxSDK/gui/gui.cpp
+++ CppHaxSDK/gui/gui.cpp
@@ -4,41 +4,62 @@
 #include <windows.h>
 #include <TlHelp32.h>
 
+// opengl
+#pragma comment(lib, "OpenGL32.lib")
+
+// directx 9
 #include <d3d9.h>
+#pragma comment(lib, "D3d9.lib")
+
+//directx 10
+#include <d3d10.h>
+#include <dxgi.h>
+#pragma comment(lib, "d3d10.lib")
+#pragma comment(lib, "dxgi.lib")
 
 #include "../logger/logger.h"
 
 #ifdef _WIN64
 #include "../third_party/detours/x64/detours.h"
+#pragma comment(lib, "third_party/detours/x64/detours.lib")
 #else
 #include "../third_party/detours/x86/detours.h"
+#pragma comment(lib, "third_party/detours/x86/detours.lib")
 #endif
 
 #include "../third_party/imgui/imgui.h"
 #include "../third_party/imgui/imgui_internal.h"
 #include "../third_party/imgui/backend/imgui_impl_dx9.h"
+#include "../third_party/imgui/backend/imgui_impl_dx10.h"
 #include "../third_party/imgui/backend/imgui_impl_win32.h"
 #include "../third_party/imgui/backend/imgui_impl_opengl3.h"
 
-using swapBuffers_t = bool(WINAPI*)(HDC);
-using reset_t = HRESULT(WINAPI*)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
-using endScene_t = HRESULT(WINAPI*)(LPDIRECT3DDEVICE9);
+using swapBuffers_t		= bool(WINAPI*)(HDC);
+using reset_t			= HRESULT(WINAPI*)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
+using endScene_t		= HRESULT(WINAPI*)(LPDIRECT3DDEVICE9);
+using present_t			= HRESULT(WINAPI*)(IDXGISwapChain*, UINT, UINT);
+using resizeBuffers_t	= HRESULT(WINAPI*)(IDXGISwapChain*, UINT, UINT, UINT, DXGI_FORMAT, UINT);
 
-static WNDPROC			oWndproc;
-static swapBuffers_t	oSwapBuffers;
-static reset_t			oReset;
-static endScene_t		oEndScene;
-static bool				g_visible = true;
+static WNDPROC					oWndproc;
+static swapBuffers_t			oSwapBuffers;
+static reset_t					oReset;
+static endScene_t				oEndScene;
+static present_t				oPresent10;
+static resizeBuffers_t			oResizeBuffers;
+static bool						g_visible = true;
+static ID3D10RenderTargetView*	g_mainRenderTargetView;
+static ID3D10Device*			g_pDevice;
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-enum GraphicsAPI {OpenGL, DirectX9};
+enum GraphicsAPI {OpenGL, D3D9, D3D10};
 
 struct ImGuiContextParams
 {
 	GraphicsAPI			api;
-	LPDIRECT3DDEVICE9	pDevice;
+	LPDIRECT3DDEVICE9	pDevice9;
 	HDC					hdc;
+	IDXGISwapChain*		pSwapChain10;
 };
 
 static LRESULT WINAPI HookedWndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -48,6 +69,11 @@ static HRESULT WINAPI HookedEndScene(LPDIRECT3DDEVICE9 pDevice);
 static void HookOpenGL();
 static void HookDirectX9();
 static void InitImGuiContext(ImGuiContextParams params);
+//directx10
+static HRESULT WINAPI HookedResizeBuffers(IDXGISwapChain* pSwapChain, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags);
+static HRESULT WINAPI HookedPresent10(IDXGISwapChain* pSwapChain, UINT syncInterval, UINT flags);
+static void HookDirectX10();
+static void CreateRenderTarget(IDXGISwapChain* pSwapChain);
 
 void haxsdk::ImplementImGui() {
 	DWORD processId = GetProcessId(GetCurrentProcess());
@@ -144,11 +170,41 @@ static void HookDirectX9() {
 	DetourTransactionCommit();
 }
 
+static void HookDirectX10() {
+	HMODULE module = GetModuleHandle("d3d10.dll");
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc = { };
+	swapChainDesc.Windowed = TRUE;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.OutputWindow = ::GetConsoleWindow();
+	swapChainDesc.SampleDesc.Count = 1;
+
+	IDXGISwapChain* pSwapChain;
+	HRESULT hr = D3D10CreateDeviceAndSwapChain(NULL, D3D10_DRIVER_TYPE_NULL, NULL, 0, D3D10_SDK_VERSION, &swapChainDesc, &pSwapChain, &g_pDevice);
+	if (hr != S_OK) {
+		LOG_ERROR << "D3D10CreateDeviceAndSwapChain() failed." << hr << LOG_FLUSH;
+		return;
+	}
+
+	void** pVTable = *reinterpret_cast<void***>(pSwapChain);
+	oPresent10 = (present_t)pVTable[8];
+	oResizeBuffers = (resizeBuffers_t)pVTable[13];
+
+	pSwapChain->Release();
+
+	DetourTransactionBegin();
+	DetourAttach(&(PVOID&)oPresent10, HookedPresent10);
+	DetourAttach(&(PVOID&)oResizeBuffers, HookedResizeBuffers);
+	DetourTransactionCommit();
+}
+
 static bool WINAPI HookedSwapBuffers(HDC hdc) {
 	static bool inited = false;
 	if (!inited) {
 		inited = true;
-		ImGuiContextParams params = { GraphicsAPI::OpenGL, 0, hdc };
+		ImGuiContextParams params = { GraphicsAPI::OpenGL, nullptr, hdc, nullptr };
 		InitImGuiContext(params);
 		LOG_INFO << "Hello from hooked wglSwapBuffers" << LOG_FLUSH;
 	}
@@ -176,7 +232,7 @@ static HRESULT WINAPI HookedEndScene(LPDIRECT3DDEVICE9 pDevice) {
 	static bool inited = false;
 	if (!inited) {
 		inited = true;
-		ImGuiContextParams params = { GraphicsAPI::DirectX9, pDevice, 0 };
+		ImGuiContextParams params = { GraphicsAPI::D3D9, pDevice, 0, nullptr };
 		InitImGuiContext(params);
 		LOG_INFO << "Hello from hooked EndScene" << LOG_FLUSH;
 	}
@@ -193,6 +249,45 @@ static HRESULT WINAPI HookedEndScene(LPDIRECT3DDEVICE9 pDevice) {
 	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
 
 	return oEndScene(pDevice);
+}
+
+static HRESULT WINAPI HookedPresent10(IDXGISwapChain* pSwapChain, UINT syncInterval, UINT flags) {
+	static bool inited = false;
+	if (!inited) {
+		inited = true;
+		ImGuiContextParams params = { GraphicsAPI::D3D10, nullptr, 0, pSwapChain };
+		InitImGuiContext(params);
+		LOG_INFO << "Hello from hooked Present10" << LOG_FLUSH;
+	}
+	if (!g_mainRenderTargetView) {
+		CreateRenderTarget(pSwapChain);
+	}
+	ImGui_ImplDX10_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+	ImGui::ShowDemoWindow();
+	ImGui::EndFrame();
+	ImGui::Render();
+	g_pDevice->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
+	ImGui_ImplDX10_RenderDrawData(ImGui::GetDrawData());
+}
+
+static HRESULT WINAPI HookedResizeBuffers(IDXGISwapChain* pSwapChain, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags) {
+if (g_mainRenderTargetView) {
+		g_mainRenderTargetView->Release();
+	}
+	CreateRenderTarget(pSwapChain);
+}
+
+static void CreateRenderTarget(IDXGISwapChain* pSwapChain) {
+	ID3D10Texture2D* pBackBuffer = NULL;
+	pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+	if (pBackBuffer) {
+		DXGI_SWAP_CHAIN_DESC sd;
+		pSwapChain->GetDesc(&sd);
+		g_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+		pBackBuffer->Release();
+	}
 }
 
 static LRESULT WINAPI HookedWndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -221,13 +316,22 @@ static void InitImGuiContext(ImGuiContextParams params) {
 		ImGui_ImplWin32_Init(hwnd);
 		ImGui_ImplOpenGL3_Init();
 	}
-	if (params.api == GraphicsAPI::DirectX9) {
+	if (params.api == GraphicsAPI::D3D9) {
 		D3DDEVICE_CREATION_PARAMETERS creationParams;
-		params.pDevice->GetCreationParameters(&creationParams);
+		params.pDevice9->GetCreationParameters(&creationParams);
 		hwnd = creationParams.hFocusWindow;
 		ImGui::CreateContext();
 		ImGui_ImplWin32_Init(hwnd);
-		ImGui_ImplDX9_Init(params.pDevice);
+		ImGui_ImplDX9_Init(params.pDevice9);
+	}
+	else {
+		DXGI_SWAP_CHAIN_DESC swapChainDesc;
+		params.pSwapChain10->GetDevice(IID_PPV_ARGS(&g_pDevice));
+		params.pSwapChain10->GetDesc(&swapChainDesc);
+		hwnd = swapChainDesc.OutputWindow;
+		ImGui::CreateContext();
+		ImGui_ImplWin32_Init(hwnd);
+		ImGui_ImplDX10_Init(g_pDevice);
 	}
 
 	ImGuiIO& io = ImGui::GetIO();
--- CppHaxSDK/gui/gui.h
+++ CppHaxSDK/gui/gui.h
@@ -1,11 +1,4 @@
 #pragma once
-#pragma comment(lib, "OpenGL32.lib")
-#pragma comment(lib, "D3d9.lib")
-#ifdef _WIN64
-#pragma comment(lib, "third_party/detours/x64/detours.lib")
-#else
-#pragma comment(lib, "third_party/detours/x86/detours.lib")
-#endif
 
 namespace haxsdk {
 	void ImplementImGui();
